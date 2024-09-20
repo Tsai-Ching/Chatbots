6d371677ce57e8d170907c0cb5fab8dba01ac2f6
Emma Hsu <Emma.Hsu.hpc@hetw.onmicrosoft.com>
2024-09-20 15:21:45 +0800

    修改logo路由 調整版面

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { InputTextModule } from 'primeng/inputtext';
 import { CardModule } from 'primeng/card';
 import { ButtonModule } from 'primeng/button';
+import { TopbarComponent } from '../topbar/topbar.component';
 
 @Component({
   selector: 'app-account',
   standalone: true,
-  imports: [InputTextModule, CardModule, ButtonModule],
+  imports: [InputTextModule, CardModule, ButtonModule, TopbarComponent],
   templateUrl: './account.component.html',
   styleUrl: './account.component.scss'
 })

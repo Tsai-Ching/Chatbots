496b890389bff63c681a9c697767eac61c896075
Emma Hsu <Emma.Hsu.hpc@hetw.onmicrosoft.com>
2024-09-20 11:14:01 +0800

    加入spinner

--- src/app/forget-pwd/forget-pwd.component.ts
+++ src/app/forget-pwd/forget-pwd.component.ts
@@ -5,11 +5,12 @@ import { RouterModule } from '@angular/router';
 import { InputTextModule } from 'primeng/inputtext';
 import { ButtonModule } from 'primeng/button';
 import { RippleModule } from 'primeng/ripple';
+import { ProgressSpinnerModule } from 'primeng/progressspinner';
 
 @Component({
   selector: 'app-forget-pwd',
   standalone: true,
-  imports: [FormsModule, RouterModule, InputTextModule, ButtonModule, RippleModule],
+  imports: [FormsModule, RouterModule, InputTextModule, ButtonModule, RippleModule, ProgressSpinnerModule],
   templateUrl: './forget-pwd.component.html',
   styleUrl: './forget-pwd.component.scss'
 })
@@ -17,15 +18,18 @@ export class ForgetPwdComponent {
   authService = inject(AuthService);
   email: string = '';
   isSent: boolean = false;
+  isLoading: boolean = false;
 
   getResetEmail() {
-
+    this.isLoading = true;
     this.authService.getResetEmail(this.email).subscribe({
       next: (response) => {
+        this.isLoading = false;
         this.isSent = true;
         console.log('請求成功', response);
       },
       error: (error) => {
+        this.isLoading = false;
         console.error('請求失敗', error);
       },
     });;
